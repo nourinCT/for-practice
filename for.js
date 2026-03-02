877111d94ccad59ae6358c06ea7e66461eae0e25
Camion Themes <camionthemes@Camions-MacBook-Pro.local>
2026-03-03 01:13:29 +0600

    first commit

---
+++ for.js
@@ -0,0 +1,72 @@
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+//
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+//
+for (let i = 10; i <= 15; i++) {
+    console.log(i);
+}
+for (let i = 10; i <= 15; i++) {
+    console.log(i);
+}
+//
+for (let i = 15; i <= 20; i++) {
+    console.log(i);
+}
+for (let i = 15; i <= 20; i++) {
+    console.log(i);
+}
+for (let i = 15; i <= 20; i++) {
+    console.log(i);
+}
+for (let i = 20; i <= 25; i++) {
+    console.log(i);
+}
+for (let i = 20; i <= 25; i++) {
+    console.log(i);
+}
+for (let i = 20; i <= 25; i++) {
+    console.log(i);
+}
+for (let i = 50; i <= 55; i++) {
+    console.log(i);
+}
+for (let i = 95; i <= 99; i++) {
+    console.log(i);
+}
+for (let i = 80; i <= 85; i++) {
+    console.log(i);
+}
+for (let i = 86; i <= 90; i++) {
+    console.log(i);
+}
+for (let i = 64; i <= 70; i++) {
+    console.log(i);
+}
+for (let i = 70; i <= 74; i++) {
+    console.log(i);
+}
+for (let i = 76; i <= 80; i++) {
+    console.log(i);
+}
+for (let i = 44; i <= 48; i++) {
+    console.log(i);
+}
+for (let i = 49; i <= 51; i++) {
+    console.log(i);
+}
+for (let i = 31; i <= 34; i++) {
+    console.log(i);
+}
+for (let i = 35; i <= 39; i++) {
+    console.log(i);
+}
